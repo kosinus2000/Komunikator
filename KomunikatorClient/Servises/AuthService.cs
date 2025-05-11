7a1b51f9e07086cac13767bf5df9102f870ad1a3
Hubert <hubert.kosiorowski1@gmail.com>
2025-05-12 00:08:16 +0200

    wykonane sprawdzenie odpowiedzi servera

--- KomunikatorClient/Servises/AuthService.cs
+++ KomunikatorClient/Servises/AuthService.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using KomunikatorClient.Models;
 using Serilog;
+using System.Net.Http.Json;
 
 namespace KomunikatorClient.Services
 {
@@ -28,19 +30,55 @@ namespace KomunikatorClient.Services
             try
             {
                 string jsonPayload = System.Text.Json.JsonSerializer.Serialize(loginRequestModel);
-                Log.Debug("AuthService: Wysyłamy Json payload {Payload}",jsonPayload);
-                
+                /*Log.Debug("AuthService: Wysyłamy Json payload {Payload}", jsonPayload);
+
                 HttpContent httpContent = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
-                
+
                 Log.Information("AuthService: Wysyłanie żądania POST na adres: {Url}", loginEndpointUrl);
                 HttpResponseMessage httpResponse = await _httpClient.PostAsync(loginEndpointUrl, httpContent);
-                return true;
+
+                if (httpResponse != null)
+                {
+                    Log.Information("AuthService: Otrzymano odpowiedź od serwera ze statusem: {StatusCode}",
+                        httpResponse.StatusCode);
+                    return httpResponse.IsSuccessStatusCode;
+                }
+                Wykonane dla celów edukacyjnych :)
+                */
+
+                // ---Automatyczne serializacja JSONa za pomoca PostAsJsonAsync---
+                Log.Information("AuthService: Wysyłanie żądania POST (używając PostAsJsonAsync) na adres: {Url} dla użytkownika {Username}", loginEndpointUrl, loginRequestModel.Username);
+                
+                HttpResponseMessage httpResponse = await _httpClient.PostAsJsonAsync(loginEndpointUrl, loginRequestModel);
+                
+                if (httpResponse != null)
+                {
+                    Log.Information("AuthService: Otrzymano odpowiedź od serwera (PostAsJsonAsync) ze statusem: {StatusCode}", httpResponse.StatusCode);
+                    return httpResponse.IsSuccessStatusCode;
+                }
+                
+                return false;
+
             }
-            catch (Exception e)
+            catch (HttpRequestException e)
             {
-                Console.WriteLine(e);
-                throw;
+                Log.Error("AuthService: Błąd żądania http podczas logowania logowania dla {Username}",
+                    loginRequestModel.Username);
+                return false;
             }
+            catch (JsonException e)
+            {
+                Log.Error("AuthService: Błąd wysłanego json podczas przygotowania żądania dla {Username}",
+                    loginRequestModel.Username);
+                return false;
+            }
+            catch(Exception e)
+            {
+                Log.Error("AuthService: Nieoczekiwany bład podczas logowania użytkownika {Username}", loginRequestModel.Username);;
+                return false;
+            }
+            
+            
         }
     }
 }
