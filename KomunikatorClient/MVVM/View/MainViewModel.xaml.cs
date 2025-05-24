f257d4b66482b4900f57e0fc812e79913f82fce2
Hubert <hubert.kosiorowski1@gmail.com>
2025-05-24 02:15:48 +0200

    Inerfejs gotowy

--- KomunikatorClient/MVVM/View/MainViewModel.xaml.cs
+++ KomunikatorClient/MVVM/View/MainViewModel.xaml.cs
@@ -1,18 +1,63 @@
 using System.Collections.ObjectModel;
 using System.Net.Mime;
+using KomunikatorClient.Core;
 using KomunikatorClient.MVVM.Model;
 
 namespace KomunikatorClient.MVVM.View;
 
-public partial class MainViewModel
+public partial class MainViewModel : ObservableObject
 {
     public ObservableCollection<MessageModel> Messages { get; set; }
     public ObservableCollection<ContactModel> Contacts { get; set; }
+
+    public RelayCommand SendCommand { get; set; }
+
+    private ContactModel _selectedContact;
+
+    public ContactModel SelectedContact
+    {
+        get { return _selectedContact; }
+        set
+        {
+            _selectedContact = value;
+            OnPropertyChanged();
+        }
+    }
+    
+
+
+    private string _message;
+
+    public string Message
+    {
+        get { return _message; }
+        set
+        {
+            {
+                _message = value;
+            }
+            OnPropertyChanged();
+        }
+    }
 
     public MainViewModel()
     {
         Messages = new ObservableCollection<MessageModel>();
         Contacts = new ObservableCollection<ContactModel>();
+
+        SendCommand = new RelayCommand(o =>
+        {
+            if (string.IsNullOrWhiteSpace(Message))
+                return;
+            Messages.Add(new MessageModel
+            {
+                Message = Message,
+                FirstMessage = false,
+
+
+            });
+            Message = string.Empty;
+        });
 
         Messages.Add(new MessageModel
         {
